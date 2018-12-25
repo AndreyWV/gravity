f1bb736277e7c4af2880580e1d1a91b22aa44110
andreywv <andrey.w.v@gmail.com>
2018-12-25 23:49:47 +0300

    Added scrollable and rotated camera

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+const OrbitControls = require('three-orbit-controls')(THREE)
 import { PhysicalBody } from './physical-body.class';
 import { createScene } from './scene';
 
@@ -7,17 +8,43 @@ import { createScene } from './scene';
 const { scene, camera, renderer } = createScene();
 
     
-var geometry = new THREE.SphereGeometry(3, 50, 50, 0, Math.PI * 2, 0, Math.PI * 2);
+var geometry = new THREE.SphereGeometry(1, 50, 50, 0, Math.PI * 2, 0, Math.PI * 2);
 var material = new THREE.MeshNormalMaterial();
 var sphere = new THREE.Mesh( geometry, material );
+initControls();
+
 scene.add( sphere );
+drawLines();
 
 var render = function () {
   requestAnimationFrame(render);
 
   sphere.rotation.y += 0.01;
-
+  
   renderer.render(scene, camera);
 };
 
 render();
+
+function drawLines() {
+  var material = new THREE.LineBasicMaterial( { color: 0x0000ff } );
+  var geometry = new THREE.Geometry();
+  geometry.vertices.push(new THREE.Vector3( -10, 0, 0) );
+  geometry.vertices.push(new THREE.Vector3( 0, 10, 0) );
+  geometry.vertices.push(new THREE.Vector3( 10, 0, 0) );
+  var line = new THREE.Line( geometry, material );
+  scene.add( line );
+}
+
+function initControls() {
+  const CONTROLS = new OrbitControls(camera);
+  CONTROLS.minPolarAngle = Math.PI * 1 / 4;
+  CONTROLS.maxPolarAngle = Math.PI * 3 / 4;
+  CONTROLS.minDistance = 1;
+  CONTROLS.maxDistance = 100000;
+  // CONTROLS.autoRotate = true;
+  // CONTROLS.autoRotateSpeed = -1.0;
+  CONTROLS.update();
+
+  // const MOUSE = new THREE.Vector2();
+}
--- src/scene.ts
+++ src/scene.ts
@@ -2,9 +2,9 @@ import * as THREE from 'three';
 
 export function createScene() {
   const scene = new THREE.Scene();
-  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-  camera.position.set(-10, -10, -10);
-  camera.lookAt( new THREE.Vector3(0, 0, 0) );
+  const camera = new THREE.PerspectiveCamera(1, window.innerWidth / window.innerHeight, 0.1, 10000 );
+  camera.position.set(-100, -100, -100);
+  camera.lookAt(scene.position);
   
   const renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
